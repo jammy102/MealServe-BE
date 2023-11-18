d0e2b09052e52d9d8074da599227ef2663d5a248
Seoyoon <wideskyinme@gmail.com>
2023-11-18 11:15:39 +0900

    init commit

---
+++ src/test/java/com/example/mealserve/MealserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mealserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
